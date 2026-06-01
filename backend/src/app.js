c952384da56dcb28b2c085728c3899ec1c72e530
Akraj <akraj8540@gmail.com>
2026-06-01 21:41:04 +0530

    fix vercel build stable

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,9 @@ const errorHandler = require("./middlewares/errorMiddleware");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 
 app.use("/api/tickets", ticketRoutes);
