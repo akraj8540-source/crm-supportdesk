688d5117da156f08087e45c40cd847d5a979e7e9
Akraj <akraj8540@gmail.com>
2026-06-02 16:05:55 +0530

    Cors issue is fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
     origin:[
-        "http://localhost:5173"
+        "https://crm-supportdesk-t314-liard.vercel.app/"
     ],
     credentials: true,
 }));
