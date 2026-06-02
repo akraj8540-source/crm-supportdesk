0d46e1c268a1cbc0515a07cc41372876db3c4e86
Akraj <akraj8540@gmail.com>
2026-06-02 09:55:37 +0530

    fix bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ const errorHandler = require("./middlewares/errorMiddleware");
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:[
+        "http://localhost:5173",
+        "https://crm-supportdesk-frontend.vercel.app",
+        "https://crm-supportdesk-onrender.com"
+    ],
     credentials: true,
 }));
 app.use(express.json());
